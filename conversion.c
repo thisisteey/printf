2d85672e794302909fb123c3758ba5e7bb9aa239
thisisteey <dadataiwo82@yahoo.com>
2023-09-20 16:17:30 -0700

    File containing conversion functions for the custom printf function

--- conversion.c
+++ conversion.c
@@ -1,24 +1,31 @@
 #include "main.h"
 
 /**
- * convert - converts number and base into string
+ * convert - converts number and base to string
  * @num: input number
  * @base: input base
  * @lowercase: flag if hexa values need to be lowercase
- * @a: pointer to flag_t structure containing formatting flags
+ * @f: pointer to the struct width
+ * @d: pointer to the struct precision
+ * @c: pointer to the struct length
  * Return: return pointer to the string
  */
-char *convert(unsigned long int num, int base, int lowercase, flags_t *a)
+char *convert(unsigned long int num, int base, int lowercase, flags_t *a, wid_t *f, pre_dot *d, len_t *c)
 {
 	static char *rep;
-	static char buffer[50];
+	static char buffer[1024];
 	char *ptr;
+
+	(void)a;
+	(void)f;
+	(void)d;
+	(void)c;
 
 	rep = (lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
 
-	ptr = &buffer[49];
+	ptr = &buffer[1023];
 
 	*ptr = '\0';
 
@@ -26,17 +33,6 @@ char *convert(unsigned long int num, int base, int lowercase, flags_t *a)
 		*--ptr = rep[num % base];
 		num /= base;
 	} while (num != 0);
-
-	if (a->precision > 0)
-	{
-		int padding = a->precision - (int)(strlen(ptr));
-
-		while (padding > 0)
-		{
-			*--ptr = '0';
-			padding--;
-		}
-	}
 
 	return (ptr);
 }
