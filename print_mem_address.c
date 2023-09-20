820c48f1972e77515c4a601d0ba49a7adb4fd8f2
AquillaAf <aquillaafintinni@gmail.com>
2023-09-20 23:14:38 +0000

    handle mem address

--- print_mem_address.c
+++ print_mem_address.c
@@ -5,21 +5,21 @@
  * in hexadecimal format
  * @b: va_list arguments from _printf
  * @a: pointer to the struct flags
+ * @f: pointer to the struct width
+ * @d: pointer to the struct precision
+ * @c: pointer to the strcut length
  * Return: number of characters printed
  */
-int print_mem_address(va_list b, flags_t *a)
+int print_mem_address(va_list b, flags_t *a, wid_t *f, pre_dot *d, len_t *c)
 {
 	char *s;
 	unsigned long int p = va_arg(b, unsigned long int);
 
 	int count = 0;
 
-	if (a->precision == 0 && p == 0)
-		return (_puts(""));
-
 	if (!p)
 		return (_puts("(nil)"));
-	s = convert(p, 16, 1, a);
+	s = convert(p, 16, 1, a, f, d, c);
 	count += _puts("0x");
 	count += _puts(s);
 
